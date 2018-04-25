bbca04f779425b5b1cf58ddbea2c4969a92c977e
Cesar Jeanroy <cesar.jeanroy@gmail.com>
2018-04-25 00:04:14 -0400

    Displaying todos

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,6 +1,12 @@
 <template>
  <div class='container'>
- <h1>{{ name }}</h1>
+  <h1>{{ name }}</h1>
+  <div class='row'></div>
+  <div class='row'>
+    <ul id="todoList">
+      <li v-for="(data, index) in todos" :key="index"> {{ index }}: {{ data.title }}</li>
+    </ul>
+  </div>
  </div>
 </template>
 
@@ -9,15 +15,39 @@ export default {
   name: 'TodoList',
   props: {
     name: String
+  },
+  data() {
+    return {
+      todos:[
+        {title: 'Bring wetsuit', severity: 3},
+        {title: 'Sim Card', severity: 2},
+        {title: 'Passport', severity: 1},
+      ]
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.container{
+.container {
   width: 50%;
   margin: auto;
-  background-color: #f3f3f3
+  background-color: #f3f3f3;
+  min-height: 400px;
+  padding-top: 15px;
+}
+
+#todoList {
+  float: left;
+}
+#todoList > li{
+  text-align: left;
+  display: block;
+}
+
+.row {
+  margin-top: 10px;
+  width:100%;
 }
 </style>
