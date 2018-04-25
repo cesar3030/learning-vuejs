379297342c235edb416870e689b2f0a35e96c444
Cesar Jeanroy <cesar.jeanroy@gmail.com>
2018-04-24 23:43:50 -0400

    OMGgit add -A! This is my first Vue.js Component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TodoList name="Stuff to pack"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoList from './components/TodoList.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    TodoList
   }
 }
 </script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,23 @@
+<template>
+ <div class='container'>
+ <h1>{{ name }}</h1>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+  props: {
+    name: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container{
+  width: 50%;
+  margin: auto;
+  background-color: #f3f3f3
+}
+</style>
